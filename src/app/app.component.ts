8054cffa0dd4f21d84daf2b99db4a453af134f7b
Radoslaw Mejer <radmen@desmart.com>
2016-05-14 12:21:59 +0200

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import '../style/app.scss';
+
+@Component({
+  selector: 'my-app', // <my-app></my-app>
+  template: require('./app.component.html'),
+})
+export class AppComponent {
+}
