92b4771ac0976fe1518f399e495bff7ee26148f3
Radoslaw Mejer <radmen@desmart.com>
2016-05-16 18:07:38 +0200

    Add initial state for quicker app development

--- src/bootstrap/appStore.ts
+++ src/bootstrap/appStore.ts
@@ -12,7 +12,26 @@ export interface State {
   story?: Story;
 }
 
+const initialState = <State>{
+  user: <User> {
+    id: '3c152058-1b7e-11e6-bb5d-f70370a2a08b',
+    name: 'Jon',
+  },
+
+  story: <Story> {
+    id: '56110968-1b7e-11e6-a2c7-f70370a2a08b',
+    title: 'Test story',
+    chunks: [],
+    users: [],
+  }
+};
+
+declare module 'redux' {
+  export function createStore<TState>(reducer: IReducer<TState>, initialState?: TState, enhancer?: Function): IStore<TState>;
+}
+
 export const appStore: IStore<State> = createStore(
   createReducer(),
+  initialState,
   applyMiddleware(createLogger())
 );
